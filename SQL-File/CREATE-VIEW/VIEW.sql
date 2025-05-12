97b4975e45e7312e8fe693d624241ff842a2214a
Tanaban Kob <tanabankob.tp@gmail.com>
2025-05-12 23:10:22 +0700

    Update VIEW.sql

--- SQL-File/CREATE-VIEW/VIEW.sql
+++ SQL-File/CREATE-VIEW/VIEW.sql
@@ -1,10 +1,10 @@
 CREATE VIEW invoice_jan2009 AS
 SELECT 
-	Invoicedate,
+  Invoicedate,
   billingaddress,
   billingcity,
-  *total,
-  ROUND(total * 0.07,2)  AS vat
+  total,
+  ROUND(total * 0.07,2)  AS vat,
   SUBSTR(billingcity, 1, 3) AS first_three_letters
 FROM invoices
 WHERE invoicedate BETWEEN '2009-01-06' AND '2009-01-31'
